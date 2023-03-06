3063752905dc0521f23b64ef0e161b2611f8658b
W-Vicky11 <126785491+W-Vicky11@users.noreply.github.com>
2023-03-06 18:26:54 +0800

    Add files via upload

---
+++ Assignment1/calculator/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            int n = 0;
+            double d = 0;
+            string mode="";
+
+            Console.Write("Please input an integer number:");
+            s = Console.ReadLine();
+            n = Int32.Parse(s);
+            Console.Write("Please input a double number: ");
+            s = Console.ReadLine();
+            d = Double.Parse(s);
+            Console.Write("Please input the operator:");
+            s = Console.ReadLine();
+            mode = s;
+
+            switch(mode)
+            {
+                case "+":
+                    Console.WriteLine($"n+d: {n + d} ");
+                    break;
+                case "-":
+                    Console.WriteLine($"n-d: {n - d} ");
+                    break;
+                case "*":
+                    Console.WriteLine($"n*d: {n * d} ");
+                    break;
+                case "/":
+                    Console.WriteLine($"n/d: {n / d}");
+                    break;
+            }
+
+        }
+    }
+}

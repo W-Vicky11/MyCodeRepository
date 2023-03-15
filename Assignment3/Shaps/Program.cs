0d18ac5e13f7d48c17b05358b1293bb6a07b7efb
W-Vicky11 <126785491+W-Vicky11@users.noreply.github.com>
2023-03-15 08:37:52 +0800

    Add files via upload

---
+++ Assignment3/Shaps/Program.cs
@@ -0,0 +1,78 @@
+﻿//长方形
+Rectangle rectangle=new Rectangle();
+rectangle.Width = 2;
+rectangle.Height = 3;
+Console.WriteLine(rectangle.GetArea());
+Console.WriteLine(rectangle.IfReal());
+
+//正方形
+Square square=new Square();
+square.Side = 0;
+Console.WriteLine(square.GetArea());
+Console.WriteLine(square.IfReal());
+
+//三角形
+Triangle triangle=new Triangle();
+triangle.Width = 2;
+triangle.Height = 3;
+Console.WriteLine(triangle.GetArea());
+Console.WriteLine(triangle.IfReal());
+
+//抽象类
+public abstract class Shaps
+{
+    public string Type { get; set; }
+    public abstract double GetArea();
+    public abstract bool IfReal();
+}
+//长方形
+public class Rectangle:Shaps
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+   
+    public override double GetArea()
+    {
+        return Width * Height;
+    }
+    public override bool IfReal()
+    {
+        if (Width<=0| Height<=0) return false;
+        else return true;
+    }
+};
+
+//正方形
+class Square:Shaps
+{
+    public double side;
+    public double Side
+    {
+        get { return side; }
+        set { side = value; }
+    }
+    public override double GetArea()
+    {
+       return side*side;
+    }
+    public override bool IfReal()
+    {
+        if (Side <= 0) return false;
+        else return true;
+    }
+}
+//三角形
+class Triangle:Shaps
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+    public override double GetArea()
+    {
+        return Width * Height / 2;
+    }
+    public override bool IfReal()
+    {
+        if (Width <= 0 | Height <= 0) return false;
+        else return true;
+    }
+};
---
+++ Assignment3/ShapsFactory/Program.cs
@@ -0,0 +1,110 @@
+﻿Shaps shap1,shap2,shap3,shap4,shap5,shap6,shap7,shap8,shap9,shap10;
+shap1 = ShapsFactory.GetShaps("Rectangle");//通过工厂类创建产品对象
+shap2 = ShapsFactory.GetShaps("Square");
+shap3 = ShapsFactory.GetShaps("Triangle");
+shap4 = ShapsFactory.GetShaps("Circular");
+shap5 = ShapsFactory.GetShaps("Square");
+shap6 = ShapsFactory.GetShaps("Triangle");
+shap7 = ShapsFactory.GetShaps("Triangle");
+shap8 = ShapsFactory.GetShaps("Square");
+shap9 = ShapsFactory.GetShaps("Circular");
+shap10 = ShapsFactory.GetShaps("Rectangle");
+Console.WriteLine(shap1.GetArea() + shap2.GetArea() + shap3.GetArea()
+    + shap4.GetArea() + shap5.GetArea() + shap6.GetArea() + shap7.GetArea()
+    + shap8.GetArea() + shap9.GetArea() + shap10.GetArea());
+abstract class Shaps
+{
+    public void MethodSame()
+    {
+        //公共方法
+    }
+    public abstract double GetArea();
+}
+
+class Rectangle : Shaps
+{
+    //实现业务方法
+    public Rectangle(double width,double height)
+    {
+        this.Width = width;
+        this.Height = height;
+    }
+    public double Width { get; set; } = 2;
+    public double Height { get; set; } = 3;
+    public override double GetArea()
+    {
+        if (IsValid() == false) throw new Exception("异常");
+       else return Width * Height;
+    }
+    public bool IsValid()
+    {
+        return Width>0&Height>0;
+    }
+}
+class Square : Shaps
+{
+    public double A { get; set; }= 1;
+    public override double GetArea()
+    {
+        if (IsValid() == false) throw new Exception("异常");
+        else return A * A;
+    }
+    public bool IsValid()
+    {
+        return A > 0;
+    }
+}
+class Triangle : Shaps
+{
+    public double Width { get; set; } = 3;
+    public double Height { get; set; } = 2;
+    public override double GetArea()
+    {
+        if (IsValid() == false) throw new Exception("异常");
+        else return Width * Height / 2;
+    }
+    public bool IsValid()
+    {
+        return Width > 0&Height>0;
+    }
+}
+class Circular:Shaps
+{
+    public double R { get; set; } = 5;
+    public override double GetArea()
+    {
+        if (IsValid() == false) throw new Exception("异常");
+        else return R * R*Math.PI;
+    }
+    public bool IsValid()
+    {
+        return R > 0;
+    }
+}
+
+
+class ShapsFactory
+{
+    public static Shaps GetShaps(string arg)
+    {
+        Shaps shap = null;
+        if(arg.Equals("Rectangle"))
+        {
+            shap = new Rectangle(2,3);
+            //初始化设置shap
+        }
+        else if(arg.Equals("Square"))
+        {
+            shap = new Square();
+        }
+        else if(arg.Equals("Triangle"))
+        {
+            shap=new Triangle();
+        }
+        else if (arg.Equals("Circular"))
+        {
+            shap = new Circular();
+        }
+        return shap;
+    }
+}

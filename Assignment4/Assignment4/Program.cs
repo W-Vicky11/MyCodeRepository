a3d16f5359f5ba5ec303e68694e0fcc946d7f65d
W-Vicky11 <126785491+W-Vicky11@users.noreply.github.com>
2023-03-23 01:08:37 +0800

    Add files via upload

---
+++ Assignment4/Assignment4/Program.cs
@@ -0,0 +1,68 @@
+﻿//整型List
+GenericList<int> intlist = new GenericList<int>();
+for(int x = 0; x < 10; x++)
+{
+    intlist.Add(x);
+}
+//打印链表元素
+intlist.Foreach(m=>Console.Write(m+" "));
+Console.WriteLine();
+//求最大值
+int max=0;
+intlist.Foreach(m => { if (max < m) max = m; });
+Console.WriteLine(max);
+//求最小值
+int min = 0;
+intlist.Foreach(m => { if (min > m) min = m; });
+Console.WriteLine(min);
+//求和
+int sum = 0;
+intlist.Foreach(m => sum += m);
+Console.WriteLine(sum);
+
+//链表节点
+public class Node<T>
+{
+    public Node<T> Next { get; set; }
+    public T Data { get; set; }
+    public Node(T t)
+    {
+        Next = null;
+        Data = t;
+    }
+}
+//泛型链表类
+public class GenericList<T>
+{
+    private Node<T> head;
+    private Node<T> tail;
+
+    public GenericList()
+    {
+        tail = head = null;
+    }
+    public Node<T> Head
+    {
+        get => head;
+    }
+    public void Add(T t)
+    {
+        Node<T> n = new Node<T>(t);
+        if (tail == null)
+        {
+            head = tail = n;
+        }
+        else
+        {
+            tail.Next = n;
+            tail = n;
+        }
+    }
+    public void Foreach(Action<T>action)
+    {
+        for(Node<T> n = head; n != null; n = n.Next)
+        {
+            action(n.Data);
+        }
+    }
+}
---
+++ Assignment4/Clock/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Data;
+
+var alarm = new AlarmClock();
+
+alarm.Tick += (sender, e) => Console.WriteLine(e.Message);
+alarm.Alarm += (sender, e) => Console.WriteLine(e.Message);
+
+Console.WriteLine("输入闹钟时间（秒）");
+int n=Convert.ToInt32(Console.ReadLine());
+alarm.alarmTime = DateTime.Now.AddSeconds(n);//当前时间加n
+Console.WriteLine("AlarmClock start!");
+
+alarm.Start();
+Console.ReadKey();
+alarm.Stop();
+Console.WriteLine("AlarmClock is stop!");
+
+//用于传递参数
+public class EventArgs 
+{
+    public string Message { get; set; }
+    public EventArgs(string message)
+    {
+        Message= message;
+    }
+}
+public class AlarmClock
+{
+    public event EventHandler <EventArgs> Tick;
+    public event EventHandler <EventArgs> Alarm;
+    private bool isRunning;
+    //定时器
+    private Timer timer;
+    //设置闹钟时间
+    public DateTime alarmTime { get; set; }
+
+    //开始闹钟
+    public void Start()
+    {
+        if(isRunning) return;
+        isRunning = true;
+        // 使用定时器，每秒触发一次 Tick 事件
+        timer = new System.Threading.Timer(
+            callback: (state) =>
+            {
+                if (DateTime.Now >= alarmTime)
+                {
+                    // 到达闹钟时间，触发 Alarm 事件
+                    Alarm(this, new EventArgs("Ring Ring Ring!!!"));
+                }
+                else
+                {
+                    // 没有到达闹钟时间，触发 Tick 事件
+                    Tick(this, new EventArgs("Tick Tock Tick Tock"));
+                }
+            },
+            state: null,
+            dueTime: 0,
+            period: 1000
+        );
+    }
+    //停止闹钟
+    public void Stop()
+    {
+        if(!isRunning) return;
+        isRunning=false;
+        timer.Dispose();
+        timer = null;
+    }
+}
